c6bf87a111abf70700dbc0761809e9ae2e602809
kendall chapman <jkc4310@gmail.com>
2023-07-07 20:40:55 -0400

    Statements exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("welcome to the number guessing game!");
+            Console.WriteLine("Try your best to guess the right number!");
+            Console.WriteLine("what would you like the upper range of the number to be?");
+
+            var userResponse = Console.ReadLine();
+            var upperLimit = int.Parse(userResponse);
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess!");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Too Low!");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Too High!");
+            }
+
+            else
+            {
+                Console.WriteLine("You Guessed It!");
+            }
+
         }
+
+
+
     }
 }
